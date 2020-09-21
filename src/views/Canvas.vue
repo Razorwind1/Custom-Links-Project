5a0503ab86eca8ba65d0fa4be4dd381bdb7bd7eb
Lauren Tillery <tillerlo@mail.uc.edu>
2020-09-21 11:29:52 -0400

    Installed Muuri and configured the drag and drop.

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -1,47 +1,97 @@
 <template>
-  <div id="canvas">
-    <div
-      v-for="(element, index) in getGridElements"
-      :key="index"
-      @click="open(element)"
-    >{{element.label}}</div>
+<div id="canvas">
+  <div class="grid">
+    <div class="item">
+      <div class="item-content">
+        <div class="my-custom-content">
+          First </div> 
+      </div>
+    </div>
+        <div class="item">
+      <div class="item-content">
+        <div class="my-custom-content">
+         Second </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="item-content">
+        <div class="my-custom-content">
+          Third </div> 
+      </div>
+    </div>
+    <div class="item">
+      <div class="item-content">
+        <div class="my-custom-content">
+          Fourth </div> 
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
-<script>
-import { mapGetters } from "vuex";
+<script src="https://cdn.jsdelivr.net/npm/muuri@0.9.3/dist/muuri.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/web-animations-js@2.3.2/web-animations.min.js"></script>
 
-export default {
-  computed: {
-    ...mapGetters(["getGridElements"]),
-  },
-  methods: {
-    open: function (element) {
-      window.shell.openExternal(element.content.address);
-    },
-  },
-};
+<script>
+
+  import 'web-animations-js';
+  import Muuri from 'muuri';
+
+  export default {
+    mounted() {
+      var grid = new Muuri('.grid', {
+        dragEnabled: true,
+        dragSort: true,
+      });
+    }, 
+  };
 </script>
 
 <style scoped>
-#canvas {
+  #canvas {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
   width: 100%;
 }
-#canvas>div{
-  width: 100%;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 10px;
-  margin: 5px;
-  padding: 5px;
-  text-align: center;
-}
-#canvas>div:hover{
-  background-color: var(--light-background-color);
-}
+  .grid {
+    position: relative;
+  }
+  .item {
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    margin: 5px;
+    z-index: 1;
+    color: #fff;
+    text-align: center;
+    align-content: center;
+    line-height: 100px;
+  }
+  .item.muuri-item-dragging {
+    z-index: 2;
+  }
+  .item.muuri-item-releasing {
+    z-index: 3;
+  }
+  .item.muuri-item-hidden {
+    z-index: 0;
+  }
+  .item-content {
+    position: relative;
+    text-align: center;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    cursor: pointer;
+  }
+  .item.muuri-item-dragging .item-content,
+  .item.muuri-item-releasing .item-content {
+    background:	#A8A8A8;
+} 
 </style>
+
+
+
+
+ 
