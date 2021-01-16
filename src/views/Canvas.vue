0d4196cdc344dfde6035ff18e1dce0d5a737cb5c
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-01-16 06:58:07 -0500

    Text overflow method improved

--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -53,7 +53,7 @@
           <div class="img-container">
             <img v-bind:src="getElementImg(element.id, element.img)" />
           </div>
-          <div class="label">{{ element.label }}</div>
+          <label class="label">{{ element.label }}</label>
         </div>
       </grid-item>
     </div>
@@ -64,7 +64,6 @@
 import VueGridLayout from "vue-grid-layout";
 import imgUrlFromBuffer from "@/js/img/imgUrlFromBuffer.js";
 import TagsWrapper from "@/components/TagsWrapper.vue";
-import shortLabel from "@/js/shortLabel.js";
 
 export default {
   data() {
@@ -84,13 +83,6 @@ export default {
       this.layout = [];
 
       gridElements.forEach((element) => {
-        let elementLabelLenght = 10;
-        if (element.pos.sizeX == 2) elementLabelLenght = 20;
-        if (element.pos.sizeX == 3) elementLabelLenght = 33;
-        if (element.pos.sizeX == 4) elementLabelLenght = 45;
-        if (element.pos.sizeX == 5) elementLabelLenght = 58;
-        if (element.pos.sizeX > 5) elementLabelLenght = 70;
-
         this.layout.push({
           id: element.id,
           x: element.pos.x,
@@ -100,7 +92,7 @@ export default {
           i: element.id,
           img: element.content.img,
           style: element.style,
-          label: shortLabel(element.content.label, elementLabelLenght),
+          label: element.content.label,
           address: element.content.address,
         });
       });
@@ -257,10 +249,12 @@ export default {
 
 .link .label {
   max-height: 20%;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
   text-align: center;
+  white-space: nowrap;
   overflow: hidden;
+  text-overflow: ellipsis;
+  cursor: pointer;
 }
 .tagsWrapper {
   flex-direction: row;
