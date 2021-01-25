3b11745b1fd71730000ae06f6e1041b63688ec93
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-01-25 16:38:41 -0500

    Enabled saving the state

--- src/App.vue
+++ src/App.vue
@@ -40,8 +40,8 @@ export default {
     }
   },
   created: function() {
-    //const state = window.ipcRenderer.sendSync("state-read");
-    //if (state) this.$store.commit("setState", state);
+    const state = window.ipcRenderer.sendSync("state-read");
+    if (state) this.$store.commit("setState", state);
 
     window.ipcRenderer.on("cmd-args", (event, args) => {
       if (args.open_dir) {
