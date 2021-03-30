d6463723176baabbc7328f78628c2469f8242094
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-03-29 21:10:02 -0400

    Custom context menu for canvas for removeing links

--- src/components/floating/ContextMenu.vue
+++ src/components/floating/ContextMenu.vue
@@ -1,10 +1,10 @@
 <template>
-  <floating :event="this.$store.state.events.contextMenu.event" class="contextMenu">
+  <floating :event="event" class="contextMenu" :class="linkType + ' ' + firedFrom()">
     <div
-      v-for="(element, index) in this.$store.state.events.contextMenu.arg"
+      v-for="(element, index) in menuItems"
       :key="index"
       @click.stop="element.click"
-      :style="element.style"
+      :class="element.class"
     >
       {{ element.label }}
     </div>
@@ -15,6 +15,31 @@
 import floating from "@/components/floating/Floating.vue";
 
 export default {
+  data() {
+    return {
+      event: this.$store.state.events.contextMenu.arg.event,
+      linkType: this.$store.state.events.contextMenu.arg.element.type,
+      menuItems: this.$store.state.events.contextMenu.arg.content,
+    };
+  },
+  methods: {
+    firedFrom: function () {
+      if (
+        this.event.path.findIndex((div) => {
+          return div.classList?.contains("vue-grid-layout");
+        }) !== -1
+      )
+        return "canvas";
+      if (
+        this.event.path.findIndex((div) => {
+          return div.hasAttribute?.("id", "side-bar");
+        }) !== -1
+      )
+        return "side-bar";
+
+      return "";
+    },
+  },
   components: {
     floating,
   },
@@ -35,7 +60,7 @@ export default {
   border-radius: 5px;
   padding: 10px;
   width: 100%;
-  border-bottom: 1px solid var(--background-accent)
+  border-bottom: 1px solid var(--background-accent);
 }
 .contextMenu > div:last-child {
   border-bottom: none;
@@ -43,5 +68,19 @@ export default {
 .contextMenu > div:hover {
   background-color: var(--background-color);
   filter: brightness(130%);
+}
+
+.contextMenu > div.delete,
+.contextMenu > div.remove {
+  background-color: var(--alert-muted);
+}
+.contextMenu > div.open {
+  background-color: var(--button-muted);
+}
+
+.contextMenu.url > div.open-in-explorer,
+.contextMenu:not(.canvas) > div.remove,
+.contextMenu.canvas > div.delete {
+  display: none;
 }
 </style>
